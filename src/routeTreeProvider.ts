876802970cfa6da630d24b2b521fee2557eef489
Maria Clara Monteiro <mcmpacheco7@gmail.com>
2026-06-23 01:39:09 -0300

    feat: improve route tree visuals

--- src/routeTreeProvider.ts
+++ src/routeTreeProvider.ts
@@ -33,6 +33,7 @@ class EmptyRoutesTreeItem extends vscode.TreeItem {
     super('No routes found', vscode.TreeItemCollapsibleState.None);
 
     this.description = 'PHP Slim';
+    this.iconPath = new vscode.ThemeIcon('info');
     this.tooltip = 'No PHP Slim routes were found in this workspace.';
   }
 }
@@ -43,11 +44,54 @@ class RouteTreeItem extends vscode.TreeItem {
 
     this.contextValue = 'route';
     this.description = route.handler;
-    this.tooltip = `${route.method} ${route.path}`;
+    this.iconPath = getRouteIcon(route.method);
+    this.tooltip = getRouteTooltip(route);
     this.command = {
       command: 'routelens.openRoute',
       title: 'Open Route',
       arguments: [this],
     };
   }
+}
+
+function getRouteIcon(method: string): vscode.ThemeIcon {
+  const color = new vscode.ThemeColor(getRouteColorId(method));
+
+  return new vscode.ThemeIcon(getRouteIconId(method), color);
+}
+
+function getRouteIconId(method: string): string {
+  const icons: Record<string, string> = {
+    GET: 'arrow-down',
+    POST: 'add',
+    PUT: 'edit',
+    PATCH: 'diff-modified',
+    DELETE: 'trash',
+  };
+
+  return icons[method] ?? 'symbol-method';
+}
+
+function getRouteColorId(method: string): string {
+  const colors: Record<string, string> = {
+    GET: 'charts.green',
+    POST: 'charts.blue',
+    PUT: 'charts.yellow',
+    PATCH: 'charts.purple',
+    DELETE: 'charts.red',
+  };
+
+  return colors[method] ?? 'foreground';
+}
+
+function getRouteTooltip(route: Route): vscode.MarkdownString {
+  const tooltip = new vscode.MarkdownString(undefined, true);
+  const handler = route.handler ? route.handler : 'Not detected';
+
+  tooltip.appendMarkdown(`**${route.method}** \`${route.path}\`\n\n`);
+  tooltip.appendMarkdown(`Handler: \`${handler}\`\n\n`);
+  tooltip.appendMarkdown(`File: \`${route.filePath}\`\n\n`);
+  tooltip.appendMarkdown(`Line: \`${route.line}\``);
+
+  return tooltip;
 }
