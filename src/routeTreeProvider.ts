a16d60f3798ef4ee3df5a31c76b16b7eeba8d121
Maria Clara Monteiro <mcmpacheco7@gmail.com>
2026-06-23 01:35:39 -0300

    feat: show empty routes state

--- src/routeTreeProvider.ts
+++ src/routeTreeProvider.ts
@@ -2,8 +2,10 @@ import * as vscode from 'vscode';
 import { scanRoutes } from './routeScanner';
 import { Route } from './types/Route';
 
-export class RouteTreeProvider implements vscode.TreeDataProvider<RouteTreeItem> {
-  private readonly onDidChangeTreeDataEmitter = new vscode.EventEmitter<RouteTreeItem | undefined>();
+type RouteTreeNode = RouteTreeItem | EmptyRoutesTreeItem;
+
+export class RouteTreeProvider implements vscode.TreeDataProvider<RouteTreeNode> {
+  private readonly onDidChangeTreeDataEmitter = new vscode.EventEmitter<RouteTreeNode | undefined>();
 
   readonly onDidChangeTreeData = this.onDidChangeTreeDataEmitter.event;
 
@@ -11,14 +13,27 @@ export class RouteTreeProvider implements vscode.TreeDataProvider<RouteTreeItem>
     this.onDidChangeTreeDataEmitter.fire(undefined);
   }
 
-  getTreeItem(element: RouteTreeItem): vscode.TreeItem {
+  getTreeItem(element: RouteTreeNode): vscode.TreeItem {
     return element;
   }
 
-  async getChildren(): Promise<RouteTreeItem[]> {
+  async getChildren(): Promise<RouteTreeNode[]> {
     const routes = await scanRoutes();
 
+    if (routes.length === 0) {
+      return [new EmptyRoutesTreeItem()];
+    }
+
     return routes.map((route) => new RouteTreeItem(route));
+  }
+}
+
+class EmptyRoutesTreeItem extends vscode.TreeItem {
+  constructor() {
+    super('No routes found', vscode.TreeItemCollapsibleState.None);
+
+    this.description = 'PHP Slim';
+    this.tooltip = 'No PHP Slim routes were found in this workspace.';
   }
 }
 
